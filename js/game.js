9dd813d2ccd90cb650e904ee31b257c371681442
Vitoria <mvitoriaberto2@gmail.com>
2024-03-08 16:17:51 -0300

    Adicionado nome do player e tempo de jogo

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'benson',
@@ -13,7 +15,6 @@ const characters = [
     'saltitão'
 ];
 
-
 const createElement = (tag, className) => {
     const element = document.createElement(tag);
     element.className = className;
@@ -27,8 +28,13 @@ const checkEndGame = () => {
     const disabledCard = document.querySelectorAll('.disabled-card');
 
     if (disabledCard.length === 20){
-       
-        alert('Parabens, voce conseguiu!');
+       clearInterval(this.loop);
+        setTimeout (()=> {
+            
+            alert(`Parabéns, ${spanPlayer}! Seu tempo foi: ${timer.innerHTML}`);
+
+        },100);
+    
     }
 }
 
@@ -109,4 +115,18 @@ const loadGame = () => {
     });
 }
 
-loadGame();
+const startTimer = () => {
+
+    this.loop = setInterval(()=>{
+        const currentTime = +timer.innerHTML;
+        timer.innerHTML = currentTime + 1;
+    }, 1000);
+}
+
+window.onload = () => {
+
+    spanPlayer.innerHTML = localStorage.getItem('player');
+  
+    loadGame();
+}
+
